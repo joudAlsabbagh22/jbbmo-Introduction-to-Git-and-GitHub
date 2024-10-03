6787d92e00b7ea757a95c524446993312f778bd0
ReemNaw <reem@entropyai.io>
2024-10-03 20:32:42 +0300

    add prints

--- compound_interest.py
+++ compound_interest.py
@@ -13,12 +13,18 @@
 
 
 def compound_interest(p, t, r):
+
     return p * (pow((1 + r / 100), t))
 
 
 if __name__ == "__main__":
     p = float(input("Enter the principal amount: "))
+    print(f'P: {p}')
+
     t = float(input("Enter the time period: "))
+    print(f'T: {t}')
+
     r = float(input("Enter the rate of interest: "))
+    print(f'R: {r}')
 
     print("The compound interest is {:.2f}".format(compound_interest(p, t, r)))
